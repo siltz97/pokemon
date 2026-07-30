e82ac19ffea897a9feab143f41b7afd23cebe21b
Serhii Shamshur <seryozha649@gmail.com>
2026-07-30 16:28:15 +0200

    Restructure for Vercel: api/ folder + static files in root + relative API URLs

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SUPABASE_URL = os.environ["SUPABASE_URL"]
-SUPABASE_KEY = os.environ["SUPABASE_KEY"]
-PORT = int(os.environ.get("PORT", 8000))
--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "/api";
 
 async function apiFetch(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
